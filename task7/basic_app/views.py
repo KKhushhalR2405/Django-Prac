d5846fd20c4954d79d5570afd912accf754c2506
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2022-01-01 22:53:10 +0530

    1 jan

---
+++ task7/basic_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render,HttpResponse
+from django.views.generic import View,TemplateView
+# Create your views here.
+
+
+class IndexView(TemplateView):
+
+    template_name = 'index.html'
