81d65dcba47ccff657837c56afa88b8bc4e38fe3
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2022-01-04 23:52:05 +0530

    4 jan

--- task7/basic_app/models.py
+++ task7/basic_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+
+class School(models.Model):
+    name = models.CharField(max_length=256)
+    pricipal = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    school = models.ForeignKey(School, related_name = 'students', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
--- task7/task7/urls.py
+++ task7/task7/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf.urls import url
 from basic_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^$', views.IndexView.as_view(),name = 'index')
+    url(r'^$', views.IndexView.as_view(),name = 'index'),
+    url(r'^basic_app/', include('basic_app.urls'), name = 'basic_app'),
 ]
